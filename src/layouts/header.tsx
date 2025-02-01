855a63279916d3c0c3f1a20fd28801f82e085c87
orgamiryazdani <orgamiryazdani@gmail.com>
2025-02-01 21:16:34 +0330

    feat: Complete logout

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -14,19 +14,28 @@ const menuItems = [
 ];
 
 const Header: React.FC = () => {
+  const logOutHandler = () => {
+    localStorage.removeItem("accessToken");
+    window.location.reload();
+  };
+
   return (
     <header className='w-full h-[9vh] flex items-center justify-between px-7 border-b border-gray-500 text-xl'>
-      <div className="flex gap-x-10">
-      {menuItems.map(({ id, title, to }) => (
-        <NavLink
-          key={id}
-          className={({ isActive }) => (isActive ? "text-sky-600" : "")}
-          to={to}>
-          {title}
-        </NavLink>
-      ))}
+      <div className='flex gap-x-10'>
+        {menuItems.map(({ id, title, to }) => (
+          <NavLink
+            key={id}
+            className={({ isActive }) => (isActive ? "text-sky-600" : "")}
+            to={to}>
+            {title}
+          </NavLink>
+        ))}
       </div>
-      <div className="text-red-500 cursor-pointer">logout</div>
+      <div
+        onClick={logOutHandler}
+        className='text-red-500 cursor-pointer'>
+        logout
+      </div>
     </header>
   );
 };
